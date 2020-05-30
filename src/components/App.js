95347760f146f6fe959d3e4901f8cbaaa0ffc067
Veronica Stork <zealous.anemone@gmail.com>
2020-05-29 21:41:12 -0400

    When opened, app fetches random book from NYT bestseller list

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,33 @@ import ResponseButton from './ResponseButton';
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookCover: null,
+      bookDescription: null,
+      toRead: [],
+      notToRead: [],
+    };
+    this.getRandomNumber = this.getRandomNumber.bind(this);
+  }
+
+  getRandomNumber(num) {
+    return Math.floor(Math.random() * num);
+  }
+
+  componentDidMount() {
+    fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=jM07dApGlEAKNArQnG0PYJbONw8a4HWD')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          const bookList = result.results.books;
+          const randomBook = bookList[this.getRandomNumber(bookList.length)];
+          console.log(randomBook);
+        }
+      )
+  }
+
   render() {
     return (
       <div>

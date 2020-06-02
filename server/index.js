00291d34126ea207ecd6bf34ff689aa338fd8a6f
Veronica Stork <zealous.anemone@gmail.com>
2020-06-02 11:46:43 -0400

    Another attempt at connecting to the db, this time w/ middleware like in our db assignment

--- server/index.js
+++ server/index.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const path = require('path');
+const { Pool } = require('pg');
+
 const app = express();
 const port = process.env.PORT || 3000;
 const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
-const { Pool } = require('pg');
 
-
-const connectionString = {
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+});
+
+const getBooks = (req, res, next) => {
+  const text = 'SELECT * FROM public.books';
+  pool.query(text, (err, response) => {
+    if (err) {
+      return next(err);
+    }
+    res.locals.books = response.rows;
+    console.log(res.locals.books);
+    next();
+  });
 };
-
-const pool = new Pool(connectionString);
-
-pool.on('connect', () => console.log('connected to db'));
-
 
 // client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
 //   console.log(err ? err.stack : res.rows[0].message);
@@ -25,9 +31,12 @@ pool.on('connect', () => console.log('connected to db'));
 // client.connect();
 
 app.use(express.static(DIST_DIR));
+
 app.get('/', (req, res) => {
   res.sendFile(HTML_FILE);
 });
+
+app.get('/books', getBooks, (req, res) => res.status(200).json(res.locals.books));
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);

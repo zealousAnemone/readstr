6f5bca7aa3a150eb55247b9b54c4360e6133dda5
Veronica Stork <zealous.anemone@gmail.com>
2020-06-02 21:48:04 -0400

    ...

--- src/components/BookCover.js
+++ src/components/BookCover.js
@@ -10,6 +10,7 @@ class BookCover extends React.Component {
 
   componentDidMount() {
     const bookUrl = `https://www.googleapis.com/books/v1/volumes?q=isbn:${this.props.isbn}`;
+    console.log(this.props.isbn);
     fetch(bookUrl)
       .then(res => res.json())
       .then((result) => {

77b2eb7ce43c5a580b8b2baee36510ed6130826d
Veronica Stork <zealous.anemone@gmail.com>
2020-06-02 21:41:08 -0400

    Changed BookCover component to class component with ComponentWillMount... maybe will help fetch issue w/ Google books?

--- src/components/BookCover.js
+++ src/components/BookCover.js
@@ -1,21 +1,31 @@
 import React from 'react';
 
-const BookCover = (props) => {
-  let imgUrl;
-  console.log(props.isbn);
-  const bookUrl = `https://www.googleapis.com/books/v1/volumes?q=isbn:${props.isbn}`;
-  fetch(bookUrl)
-    .then(res => res.json())
-    .then((result) => {
-      console.log(`Result: ${JSON.stringify(result)}`);
-      imgUrl = result.items[0].volumeInfo.imageLinks.thumbnail;
-      console.log(`imgUrl: ${imgUrl}`);
-    })
-  return (
-    <div>
-      <img src={imgUrl} />
-    </div>
-  )
+class BookCover extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imgUrl: null,
+    }
+  }
+
+  componentDidMount() {
+    const bookUrl = `https://www.googleapis.com/books/v1/volumes?q=isbn:${props.isbn}`;
+    fetch(bookUrl)
+      .then(res => res.json())
+      .then((result) => {
+        console.log(`Result: ${JSON.stringify(result)}`);
+        this.setState({
+          imgUrl: result.items[0].volumeInfo.imageLinks.thumbnail,
+        })
+      })
+  }
+  render() {
+    return (
+      <div>
+        <img src={imgUrl} />
+      </div>
+    )
+  }
 }
 
 export default BookCover;

3e470feb687dc95eeb9bc396942a54aba49739af
Veronica Stork <zealous.anemone@gmail.com>
2020-06-02 22:14:45 -0400

    again...

--- src/components/BookCover.js
+++ src/components/BookCover.js
@@ -3,7 +3,7 @@ import React from 'react';
 const BookCover = (props) => {
     return (
       <div>
-        <img src={this.props.imgUrl} />
+        <img src={props.imgUrl} />
       </div>
     )
 }

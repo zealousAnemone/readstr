dd160a62273015622257ce889c1d43dbad1522e8
Veronica Stork <zealous.anemone@gmail.com>
2020-06-02 22:27:06 -0400

    fixed small error

--- src/components/BookDescription.js
+++ src/components/BookDescription.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const BookDescription = (props) => {
   return (
-    <h4>{props.title}</h4>
-    <p id="book-description">{props.description}</p>
+    <div>
+      <h4>{props.title}</h4>
+      <p id="book-description">{props.description}</p>
+    </div>
+    
   )
 }
 
